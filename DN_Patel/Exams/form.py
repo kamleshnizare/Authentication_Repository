4f1eb6a04fe86554a3476d66058378b1d8e2696b
kamleshnizare <143099644+kamleshnizare@users.noreply.github.com>
2024-02-15 18:55:08 +0530

    Update form.py

--- DN_Patel/Exams/form.py
+++ DN_Patel/Exams/form.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'username', 'password')
+        fields = ('first_name', 'last_name', 'email', 'username', 'password','password)
